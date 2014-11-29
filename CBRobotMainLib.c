d5fb2909eff410ffa072b39fae31e518cec7aa48
C Barber <Christopher_barber_2014@hotmail.com>
2014-11-28 22:40:50 -0800

    Made a log scale for joystick/motor mapping, added high and low speed modes based on joystick buttons, also added program for prototype kickstand kicker toggled on button "Back"

--- CBRobotMainLib.c
+++ CBRobotMainLib.c
@@ -1,30 +1,91 @@
-const int JoystickMin = 8;
-const int JoystickMax = 125;
-const int DriveMotorMin = 10;
-const int DriveMotorMax = 90;
 
 
 
-int CBMap(int OrigAxis)
+// scaleJoystick
+// Function to scale a joystick value using a logarithmic like scale with a dead
+// band at zero.
+// Most of the adjustment range is used for fine control over low power settings.
+// The extreme end of the range provide coarse control over high power.
+// Large dead band around center point.
+// Above makes it easier to control robot at slow speeds.
+// Joystick values range from -128 to +127.
+// Speed/power settings for Tetrix motors range from -100 to +100
+// The physical range of motion of a joystick is quite small and it is sometimes
+// hard to control slow speeds. So another capability of this program to apply
+// a "logarithmic" scale to the joystick settings.
+// Addapted from RobotC.net forum post by Emilhem
+
+int CBMap(int Joy1, bool FastMode)
 {
-	int Axis = RB_abs(OrigAxis);
-	float Convertion = ((float) (DriveMotorMax - DriveMotorMin)) / (JoystickMax - JoystickMin);
-	int MappedAxis = Convertion * (Axis-JoystickMin) + DriveMotorMin / 2;
-	MappedAxis = RB_constrain(MappedAxis, DriveMotorMin, DriveMotorMax);
-	if (Axis<JoystickMin)
+	static const int LogScaleFast[17] =
 	{
-		MappedAxis = 0;
+		0,
+		12,
+		12,
+		12,
+		12,
+		13,
+		15,
+		18,
+		22,
+		27,
+		34,
+		42,
+		52,
+		64,
+		78,
+		95,
+		95
+
+	};
+	static const int LogScaleSlow[17] =
+	{
+		0,
+		7,
+		7,
+		7,
+		7,
+		7,
+		7,
+		8,
+		8,
+		9,
+		10,
+		11,
+		13,
+		15,
+		17,
+		20,
+		20
+
+
+	};
+	int Scaled = Joy1/8;
+	if (FastMode && GetLiftEnc() < AutoDeployEncTop)
+	{
+		if (Scaled >= 0)
+			Scaled = LogScaleFast[Scaled];
+		else
+			Scaled = - LogScaleFast[ - Scaled];
 	}
-	return (MappedAxis * RB_sgn(OrigAxis));
+	else
+	{
+		if (Scaled >= 0)
+			Scaled = LogScaleSlow[Scaled];
+		else
+			Scaled = - LogScaleSlow[ - Scaled];
+	}
+	return Scaled;
 }
 
 task CBDrive()
 {
 	while(true)
 	{
-		int x = CBMap(joystick.joy1_x1); // Strafe left/right
-		int y = CBMap(joystick.joy1_y1); // move forward/back
-		int r = CBMap(joystick.joy1_x2)*3/4; // rotate in place
+		bool FastButton = (RB_Button_isHeld(ButtonState, 12) || RB_Button_isHeld(ButtonState, 11));
+		int x = CBMap(joystick.joy1_x1, FastButton); // Strafe left/right
+		int y = CBMap(joystick.joy1_y1, FastButton); // move forward/back
+		int r = CBMap(joystick.joy1_x2, FastButton)*3/4; // rotate in place
 
 		//motor value & power using x,y, and r to allow the robot to move in any combinations of directions.
 		int FL = -y - x - r;
