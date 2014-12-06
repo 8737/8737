c32be6694326e661859ac394b4ab1af914c64ab6
C Barber <Christopher_barber_2014@hotmail.com>
2014-12-06 00:16:39 -0800

    Toggle the hi/low speed modes

--- CBRobotMainLib.c
+++ CBRobotMainLib.c
@@ -1,44 +1,26 @@
 
 
 
-// scaleJoystick
-// Function to scale a joystick value using a logarithmic like scale with a dead
-// band at zero.
-// Most of the adjustment range is used for fine control over low power settings.
-// The extreme end of the range provide coarse control over high power.
-// Large dead band around center point.
-// Above makes it easier to control robot at slow speeds.
-// Joystick values range from -128 to +127.
-// Speed/power settings for Tetrix motors range from -100 to +100
-// The physical range of motion of a joystick is quite small and it is sometimes
-// hard to control slow speeds. So another capability of this program to apply
-// a "logarithmic" scale to the joystick settings.
-// Addapted from RobotC.net forum post by Emilhem
+/*
+scaleJoystick
+Function to scale a joystick value using a logarithmic like scale with a dead
+band at zero.
+Most of the adjustment range is used for fine control over low power settings.
+The extreme end of the range provide coarse control over high power.
+Large dead band around center point.
+Above makes it easier to control robot at slow speeds.
+Joystick values range from -128 to +127.
+Speed/power settings for Tetrix motors range from -100 to +100
+The physical range of motion of a joystick is quite small and it is sometimes
+hard to control slow speeds. So another capability of this program to apply
+a "logarithmic" scale to the joystick settings.
+Addapted from RobotC.net forum post by Emilhem
+*/
 
+bool FastButton = true;
 int CBMap(int Joy1, bool FastMode)
 {
 	static const int LogScaleFast[17] =
-	{
-		0,
-		12,
-		12,
-		12,
-		12,
-		13,
-		15,
-		18,
-		22,
-		27,
-		34,
-		42,
-		52,
-		64,
-		78,
-		95,
-		95
-
-	};
-	static const int LogScaleSlow[17] =
 	{
 		0,
 		7,
@@ -57,7 +39,27 @@ int CBMap(int Joy1, bool FastMode)
 		17,
 		20,
 		20
+	};
+	static const int LogScaleSlow[17] =
+	{
 
+		0,
+		12,
+		12,
+		12,
+		12,
+		13,
+		15,
+		18,
+		22,
+		27,
+		34,
+		42,
+		52,
+		64,
+		78,
+		95,
+		95
 
 	};
 	int Scaled = Joy1/8;
@@ -82,7 +84,10 @@ task CBDrive()
 {
 	while(true)
 	{
-		bool FastButton = (RB_Button_isHeld(ButtonState, 12) || RB_Button_isHeld(ButtonState, 11));
+		if (RB_Button_wasPressed(ButtonState,11)||RB_Button_wasPressed(ButtonState,12))
+		{
+			FastButton = !FastButton;
+		}
 		int x = CBMap(joystick.joy1_x1, FastButton); // Strafe left/right
 		int y = CBMap(joystick.joy1_y1, FastButton); // move forward/back
 		int r = CBMap(joystick.joy1_x2, FastButton)*3/4; // rotate in place
